d3d1bced876bb92c84fa9b12757ba6f326a301c9
André <andre@reisert-diamant.de>
2022-12-01 17:42:15 +0100

    Input parser

--- src/main/kotlin/com/github/cptblacksheep/adventofcode2022/InputParser.kt
+++ src/main/kotlin/com/github/cptblacksheep/adventofcode2022/InputParser.kt
@@ -4,7 +4,33 @@ class InputParser(resourceName: String) {
 
     private val res = javaClass.classLoader.getResource(resourceName)!!
 
-    fun parseInputLines(): List<String> {
+    fun parseLines(): List<String> {
         return res.readText().split("\n")
     }
+
+    fun parseIntLines(): List<Int> {
+        return parseLines().map { it.toInt() }
+    }
+
+    fun parseLinesGrouped(): List<List<String>> {
+        return mutableListOf<List<String>>().apply {
+            val group = mutableListOf<String>()
+            parseLines().forEach { line ->
+                if (line.isEmpty()) {
+                    add(group.toList())
+                    group.clear()
+                } else {
+                    group.add(line)
+                }
+            }
+            add(group.toList())
+        }
+    }
+
+    fun parseIntLinesGrouped(): List<List<Int>> {
+        return parseLinesGrouped().map { group ->
+            group.map { it.toInt() }
+        }
+    }
+
 }
