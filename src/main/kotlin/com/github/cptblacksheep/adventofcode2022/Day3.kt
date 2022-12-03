73903e0eae8c367089de6b0103c02317da31651c
André <andre@reisert-diamant.de>
2022-12-03 16:27:22 +0100

    Day 3

---
+++ src/main/kotlin/com/github/cptblacksheep/adventofcode2022/Day3.kt
@@ -0,0 +1,28 @@
+package com.github.cptblacksheep.adventofcode2022
+
+private val input = InputParser("input-day3.txt").parseLines()
+private val priorities = ('a'..'z') + ('A'..'Z')
+
+private fun solvePartOne() {
+    println(
+        input.sumOf { line ->
+            val compartments = line.chunked(line.length / 2)
+            val matchingItem = compartments[0].find { compartments[1].contains(it) }!!
+            priorities.indexOf(matchingItem) + 1
+        }
+    )
+}
+
+private fun solvePartTwo() {
+    println(
+        input.chunked(3).sumOf { rucksacks ->
+            val matchingItem = rucksacks[0].find { rucksacks[1].contains(it) && rucksacks[2].contains(it) }!!
+            priorities.indexOf(matchingItem) + 1
+        }
+    )
+}
+
+fun main() {
+    solvePartOne()
+    solvePartTwo()
+}
