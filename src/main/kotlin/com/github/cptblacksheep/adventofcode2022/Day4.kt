baf28edd2d2b9e5963548b7b0782153e033082a1
André <andre@reisert-diamant.de>
2022-12-04 13:24:24 +0100

    Day 4

---
+++ src/main/kotlin/com/github/cptblacksheep/adventofcode2022/Day4.kt
@@ -0,0 +1,29 @@
+package com.github.cptblacksheep.adventofcode2022
+
+private val input = InputParser("input-day4.txt").parseLines()
+    .map { line ->
+        Regex("\\d+").findAll(line).chunked(2).map {
+            (it[0].value.toInt()..it[1].value.toInt())
+        }.toList()
+    }
+
+private fun solvePartOne() {
+    println(
+        input.count { sections ->
+            sections[0].all { it in sections[1] } || sections[1].all { it in sections[0] }
+        }
+    )
+}
+
+private fun solvePartTwo() {
+    println(
+        input.count { sections ->
+            sections[0].any { it in sections[1] } || sections[1].any { it in sections[0] }
+        }
+    )
+}
+
+private fun main() {
+    solvePartOne()
+    solvePartTwo()
+}
