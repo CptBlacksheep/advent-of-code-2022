754552c04d6c071e77d7252361edf8dad5a99da7
André <andre@reisert-diamant.de>
2022-12-04 14:12:43 +0100

    Switched from println to return types

--- src/main/kotlin/com/github/cptblacksheep/adventofcode2022/Day3.kt
+++ src/main/kotlin/com/github/cptblacksheep/adventofcode2022/Day3.kt
@@ -9,10 +9,11 @@ private fun solvePartOne() = input.sumOf { line ->
     priorities.indexOf(matchingItem) + 1
 }
 
-private fun solvePartTwo() = input.chunked(3).sumOf { rucksacks ->
-    val matchingItem = rucksacks[0].find { rucksacks[1].contains(it) && rucksacks[2].contains(it) }!!
-    priorities.indexOf(matchingItem) + 1
-}
+private fun solvePartTwo() = input.chunked(3)
+    .sumOf { rucksacks ->
+        val matchingItem = rucksacks[0].find { rucksacks[1].contains(it) && rucksacks[2].contains(it) }!!
+        priorities.indexOf(matchingItem) + 1
+    }
 
 fun main() {
     println(solvePartOne())
