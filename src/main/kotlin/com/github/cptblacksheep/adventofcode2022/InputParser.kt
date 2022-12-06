bb7fec70e2e71976020e14768a83c332f6dfd6d7
André <andre@reisert-diamant.de>
2022-12-06 16:21:57 +0100

    Added parseString and parseChars

--- src/main/kotlin/com/github/cptblacksheep/adventofcode2022/InputParser.kt
+++ src/main/kotlin/com/github/cptblacksheep/adventofcode2022/InputParser.kt
@@ -4,34 +4,26 @@ class InputParser(resourceName: String) {
 
     private val res = javaClass.classLoader.getResource(resourceName)!!
 
-    fun parseLines(): List<String> {
-        return res.readText()
-            .split("\n")
-            .dropLastWhile { it.isEmpty() }
-    }
+    fun parseLines() = res.readText()
+        .split("\n")
+        .dropLastWhile { it.isEmpty() }
 
     fun parseIntLines() = parseLines().map { it.toInt() }
 
-    fun parseLinesGrouped(): List<List<String>> {
-        return mutableListOf<List<String>>().apply {
-            val group = mutableListOf<String>()
-            parseLines().forEach { line ->
-                if (line.isEmpty()) {
-                    add(group.toList())
-                    group.clear()
-                } else {
-                    group.add(line)
-                }
+    fun parseLinesGrouped() = mutableListOf<List<String>>().apply {
+        val group = mutableListOf<String>()
+        parseLines().forEach { line ->
+            if (line.isEmpty()) {
+                add(group.toList())
+                group.clear()
+            } else {
+                group.add(line)
             }
-            add(group.toList())
         }
+        add(group.toList())
     }
 
-    fun parseIntLinesGrouped(): List<List<Int>> {
-        return parseLinesGrouped().map { group ->
-            group.map { it.toInt() }
-        }
-    }
+    fun parseIntLinesGrouped() = parseLinesGrouped().map { group -> group.map { it.toInt() } }
 
     fun parseString(lineSeparator: String = "") = parseLines().joinToString(lineSeparator) { it }
 
